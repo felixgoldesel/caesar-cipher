78345dd68960883fca13e23bf0fdaf709ea1a95c
felixgoldesel <felix.kolb95@web.de>
2022-10-24 17:12:39 +0200

    decrypt + encrypt function

--- main.py
+++ main.py
@@ -1,27 +1,53 @@
 from string import ascii_lowercase
 from string import ascii_uppercase
 
-print("Geheimtext: ")
-ciphertext = input()
-decrypted = ""
-#value_counter = 1
-counter = 0
 
-#while counter <= 26:
-for i in ciphertext:
-    value_uppercase = ord(i)
-    if 65 <= ord(i) < 90:
-        decrypted += chr(value_uppercase + 1)
-    elif ord(i) >= 90:
-        value_uppercase = 65
-        decrypted += chr(value_uppercase)
-
-    ciphertext = decrypted
-print(decrypted)
-#    counter += 1
+option = input("Entschlüsseln [1] oder Verschlüsseln [2]: ")
+#LPUMBLOYBUN PU KPL RYFWAVNYHWOPL
 
 
-git remote add origin https://github.com/felixgoldesel/caesar-cipher.git
-git branch -M main
-git push -u origin main
+def decrypt(ciphertext):
+
+    counter = 1
+    decrypted = ""
+    ciphertext = ciphertext.upper()
+
+    while counter <= 26:
+        for i in ciphertext:
+            if ord(i) == 32:
+                decrypted += " "
+            elif ord(i) == 65:
+                value_uppercase = 90
+                decrypted += chr(value_uppercase)
+            elif 65 < ord(i) <= 90:
+                decrypted += chr(ord(i) - 1)
+
+        ciphertext = decrypted
+        print(decrypted + " KEY: " + str(counter))
+        decrypted = ""
+        counter += 1
+
+
+def encrypt(plaintext, key):
+
+    encrypted = ""
+    plaintext = plaintext.upper()
+
+    for i in plaintext:
+        if ord(i) + int(key) < 90:
+            encrypted += chr(ord(i) + int(key))
+        elif ord(i) + int(key) > 90:
+            value = (ord(i) + int(key)) - 90
+            encrypted += chr(64 + value)
+    print(encrypted)
+
+
+if option == "1":
+    decrypt(input("Geheimtext: "))
+elif option == "2":
+    encrypt(input("Text: "), input("Key: "))
+
+
+
+
 
